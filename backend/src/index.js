8ddeb29163e90a6ca62ece464a33e06a4c07506d
Fabricio Bedin <fabricioobedin@gmail.com>
2019-06-18 20:46:36 -0300

    adding socket.io and ending the first part of the API

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
 mongoose.connect('mongodb+srv://fabriciobedin:muzc7aFMsdL7LcF@cluster0-rihgs.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
 })
 
-const app = express()
+app.use((req, res, next) =>{
+  req.io = io
+
+  next()
+})
+
+app.use(cors())
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
 
 app.use(require('./routes'))
 
-app.listen(3333)
+server.listen(3333)

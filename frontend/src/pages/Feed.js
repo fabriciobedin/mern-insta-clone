4a090a78d453dc6f4647d0c54a51fb62d8e5835b
Fabricio Bedin <fabricioobedin@gmail.com>
2019-07-03 20:24:11 -0300

    getting posts from API and showing it on feed

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,40 +1,52 @@
 import React, { Component } from "react";
+import api from "../services/api";
 
 import "./Feed.css";
 
-import more from '../assets/more.svg'
-import like from '../assets/like.svg'
-import comment from '../assets/comment.svg'
-import send from '../assets/send.svg'
+import more from "../assets/more.svg";
+import like from "../assets/like.svg";
+import comment from "../assets/comment.svg";
+import send from "../assets/send.svg";
 
 class Feed extends Component {
+  state = {
+    feed: []
+  };
+
+  async componentDidMount() {
+    const response = await api.get("posts");
+
+    this.setState({ feed: response.data });
+  }
   render() {
     return (
       <section id="post-list">
-        <article>
-          <header>
-            <div className='user-info'>
-              <span>Fabricio Bedin</span>
-              <span className="place">Passo Fundo</span>
-            </div>
+        {this.state.feed.map(post => (
+          <article key={post._id}>
+            <header>
+              <div className="user-info">
+                <span>{post.author}</span>
+                <span className="place">Passo Fundo</span>
+              </div>
 
-            <img src={more} alt="Mais" />
-          </header>
-          <img src="http://localhost:3333/files/foto.jpg"/>
+              <img src={more} alt="Mais" />
+            </header>
+            <img src={`http://localhost:3333/files/${post.image}`} />
 
-          <footer>
-            <div className="actions">
-              <img src={like} alt="Like" />
-              <img src={comment} alt="Comment" />
-              <img src={send} alt="Send" />
-            </div>
-            <strong>984798 curtidas</strong>
-            <p>Primeira foto postada \o/
-              <span>#react #nodejs #react-native</span>
-            </p>
-          </footer>
-        </article>
-
+            <footer>
+              <div className="actions">
+                <img src={like} alt="Like" />
+                <img src={comment} alt="Comment" />
+                <img src={send} alt="Send" />
+              </div>
+              <strong>{post.likes} curtidas</strong>
+              <p>
+                {post.description}
+                <span>{post.hashtags}</span>
+              </p>
+            </footer>
+          </article>
+        ))}
       </section>
     );
   }
